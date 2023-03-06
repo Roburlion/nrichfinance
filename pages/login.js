3267cde393f50eeadb0d6d36211476dfa06ab377
Roburlion <94622793+Roburlion@users.noreply.github.com>
2023-03-06 06:35:13 -0600

    +google button for testing

--- pages/login.js
+++ pages/login.js
@@ -18,7 +18,7 @@ export default function Login () {
               appearance={{ theme: ThemeSupa }}
               theme='dark'
               supabaseClient={supabaseClient}
-              providers={['github']}
+              providers={['github', 'google']}
               socialLayout="vertical"
             />
         </div>
